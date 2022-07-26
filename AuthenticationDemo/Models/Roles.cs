84dc72f8b3956b01960a38b817853ce3e358dd13
91844 <91844@LAPTOP-G9942SOA>
2022-07-26 10:47:13 +0530

    Add project files.

---
+++ AuthenticationDemo/Models/Roles.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AuthenticationDemo.Models
+{
+    public class Roles
+    {
+        public string Admin { get; set; }
+        public string Customer { get; set; }
+    }
+}
